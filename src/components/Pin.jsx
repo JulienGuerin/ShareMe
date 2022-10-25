568bca4fa778a480bb6718e2e64ecf40d8fce240
Julien <ju_guerin@live.fr>
2022-10-25 20:57:35 +0200

    redirect to login if no user

--- src/components/Pin.jsx
+++ src/components/Pin.jsx
@@ -13,7 +13,7 @@ function Pin({ pin: { postedBy, image, _id, destination, save } }) {
     const navigate = useNavigate();
     const user = fetchUser();
 
-    const alreadySaved = !!(save?.filter((item) => item.postedBy._id === user.sub))?.length;
+    const alreadySaved = !!(save?.filter((item) => item.postedBy._id === user?.sub))?.length;
 
     const savePin = (id) => {
         if (!alreadySaved) {
@@ -23,10 +23,10 @@ function Pin({ pin: { postedBy, image, _id, destination, save } }) {
                 .setIfMissing({ save: [] })
                 .insert('after', 'save[-1]', [{
                     _key: uuidv4(),
-                    userId: user.sub,
+                    userId: user?.sub,
                     postedBy: {
                         _type: 'postedBy',
-                        _ref: user.sub
+                        _ref: user?.sub
                     }
                 }])
                 .commit()
@@ -95,7 +95,7 @@ function Pin({ pin: { postedBy, image, _id, destination, save } }) {
                                     {destination.length > 15 ? `${destination.slice(0, 15)}...` : destination}
                                 </a>
                             )}
-                            {postedBy?._id === user.sub && (
+                            {postedBy?._id === user?.sub && (
                                 <button
                                     type="button"
                                     onClick={(e) => {
